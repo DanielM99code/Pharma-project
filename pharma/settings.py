08a5258fbf6d7b5ad4062ddfea52925f37afe0ba
DanielM99code <danielmisiuk99@gmail.com>
2021-04-26 20:40:14 +0300

    Update settings.py

--- pharma/settings.py
+++ pharma/settings.py
@@ -140,11 +140,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/history/', '/products/', '/static/', '/team/', '/rate/', '/welcome/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static', 'history', 'products', 'static', 'team', 'rate', 'welcome'),
 )
 
 CART_SESSION_ID = 'cart'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
