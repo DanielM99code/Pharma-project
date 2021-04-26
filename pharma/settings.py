4898b5f292142028db6bd4cef1515a57e6c15f83
DanielM99code <danielmisiuk99@gmail.com>
2021-04-26 21:09:03 +0300

    Update settings.py

--- pharma/settings.py
+++ pharma/settings.py
@@ -144,11 +144,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/history/', '/products/', '/static/', '/team/', '/rate/', '/welcome/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static', 'history', 'products', 'static', 'team', 'rate', 'welcome'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 CART_SESSION_ID = 'cart'
