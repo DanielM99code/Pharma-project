fc7d5b0e50811f73256148bc5832aaf7852802a6
DanielM99code <danielmisiuk99@gmail.com>
2021-04-26 21:22:04 +0300

    Update settings.py

--- pharma/settings.py
+++ pharma/settings.py
@@ -147,7 +147,7 @@ STATIC_URL = '/static/', '/rate/', '/history/', '/products/', '/team/', '/testim
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static', 'rate', 'history', 'products', 'team', 'testimonials', 'welcome'),
 )
 
 CART_SESSION_ID = 'cart'
