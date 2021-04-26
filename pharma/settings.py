3448d0f6861a15664b49794794164de13e423ac6
DanielM99code <danielmisiuk99@gmail.com>
2021-04-26 19:39:45 +0300

    Update settings.py

--- pharma/settings.py
+++ pharma/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = ')*#rc0$!82w1th6+p+0bntnpk+7rhxf9nx)$la-30^&)5)w6!0'
+
 
 DB_NAME = os.environ.get('DB_NAME')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
@@ -28,7 +28,7 @@ DB_HOST = os.environ.get('DB_HOST')
 DB_USER = os.environ.get('DB_USER')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['pharma250421.herokuapp.com']
 
